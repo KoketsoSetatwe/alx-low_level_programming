db568ea0d9bae72273a77c1be95fc8e5479bf6be
KoketsoSetatwe <koketsosetatwe86@gmail.com>
2023-09-13 02:21:09 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,19 @@
-#include "main.h"
-
-/**
- * print_alphabet
- * Return: void
+/*
+ * File: 1-alphabet.c
+ * Auth: Brennan D Baraban
  */
 
+#include "holberton.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ */
 void print_alphabet(void)
 {
+	char letter;
 
-	char i;
-        
-	for (i = 'a' ; i <= 'z' ; i++)
-	{
-		_putchar(i);
-	}
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
 	_putchar('\n');
-
 }
-
