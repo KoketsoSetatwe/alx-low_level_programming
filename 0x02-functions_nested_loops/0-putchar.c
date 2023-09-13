b2890e2c8008da783e863a66d7cba31f5c7dc6de
KoketsoSetatwe <koketsosetatwe86@gmail.com>
2023-09-13 01:39:35 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,21 @@
 #include "main.h"
 
 /**
-* main - function that prints _putchar 
-* Return: return 0;
-*/
+ * main - A print statement followed by a new line
+ * return: Always return 0 (Success)
+ * main - A print statement followed by a new line.
+ *
+ * Return: Always 0.
+ */
 
 int main(void)
 {
 	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
+@@ -15,5 +16,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+return (0);
+
+	return (0);
 }
