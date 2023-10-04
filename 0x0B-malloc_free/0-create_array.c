1bf1365dd764a8220949ff37e7ca864bca96662c
KoketsoSetatwe <koketsosetatwe86@gmail.com>
2023-10-04 12:50:21 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,35 +4,34 @@
 
 /**
  * main - A  function that creates an array of chars,
- * 	  and initializes it with a specific char.
+ * and initializes it with a specific char.
  * @size: unsigned int type
- * @s: char type
- * 
- * Return: Pointer to the array, or 0 if it fails
+ * @c: char type
+ *
+ * Return: Pointer to the array, or  0 if it fails
  */
 
-char *create_array(unsigned int size, char c)
+char *create_array(unsigned int size, char c);
 {
-	unsigned int i;
-	char *s;
+unsigned int i;
+char *s;
 
-	if (size == 0)
-	{
-		return (NULL);
-	}
+if (size == 0)
+{
+return (NULL);
+}
+s = malloc((size) * sizeof(char));
 
-	s = malloc((size) * sizeof(char));
-
-	if (s == 0)
-	{
-		return (NULL);
-	}
-	i = 0;
-	while (i < size)
-	{
-		s[i] = c;
-		i++;
-	}
-	s[i] = '\0';
-	return (s);
+if (s == 0)
+{
+return (NULL);
+}
+i = 0;
+while (i < size)
+{
+s[i] = c;
+i++;
+}
+s[i] = '\0';
+return (s);
 }
