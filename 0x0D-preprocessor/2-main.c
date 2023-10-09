6a1207f5bf0fb5c520469308f12bb1338de6461f
KoketsoSetatwe <koketsosetatwe86@gmail.com>
2023-10-09 08:10:48 -0700

    0x0D. C - Preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,17 +1,14 @@
-#include "0-object_like_macro.h"
-#include "0-object_like_macro.h"
 #include <stdio.h>
 
 /**
- * main - check the code
+ * main - Prints the name of the file of the program
+ *        was compiled from, followed by a new line.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    int s;
+	printf("%s\n", __FILE__);
 
-    s = 98 + SIZE;
-    printf("%d\n", s);
-    return (0);
+	return (0);
 }
