64ef263f063ec73dcefda4f81b6bc5df82208c26
KoketsoSetatwe <koketsosetatwe86@gmail.com>
2023-10-17 02:20:37 -0700

    Updated

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -22,27 +22,4 @@ int sum_them_all(const unsigned int n, ...)
 	va_end(nums);
 
 	return (sum);
-#ifndef DOG_H
-#define DOG_H
-
-/**
- * struct dog - A dog's basic attributes
- * @name: The name of the dog
- * @age: The age of the dog
- * @owner: The owner of the dog
- * Description: Dog's basic attributes
- */
-struct dog
-{
-char *name;
-float age;
-char *owner;
-
-};
-
-typedef struct dog dog_t;
-
-/* any other function declarations or structures go here */
-
-#endif /* DOG_H */
 }
