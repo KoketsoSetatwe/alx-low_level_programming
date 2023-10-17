ab8b7d1dfef1fc4ed98f97522f28f4cf8b5370fb
KoketsoSetatwe <koketsosetatwe86@gmail.com>
2023-10-17 05:40:44 -0700

    Updated

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string to stdout.
+ * @s: The string to be printed.
+ */
+void _puts(char *s)
+{
+	while (*s != '\0')
+	{
+		_putchar(*s++);
+	}
+		_putchar('\n');
+}
