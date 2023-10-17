c545d41d27b8674b08c4256e985063b77358e93b
KoketsoSetatwe <koketsosetatwe86@gmail.com>
2023-10-17 02:55:10 -0700

    Updated

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,53 +1,18 @@
+#include "holberton.h"
 #include <stdlib.h>
-#include "main.h"
 
 /**
- * string_nconcat -  Concatenates two strings
- * @s1: string to append to
- * @s2: String to concatenates from
- * @n: the amount of memory from s2 to concatenate to s1
+ * malloc_checked - Allocates memory using malloc.
+ * @b: The number of bytes to be allocated.
  *
- * Return: pointer to the string results
+ * Return: A pointer to the allocated memory.
  */
-
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+void *malloc_checked(unsigned int b)
 {
-	unsigned int i = 0, j = 0, k = 0, l = 0;
-	char *str;
+	void *mem = malloc(b);
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+	if (mem == NULL)
+		exit(98);
 
-	while (s1[i])
-		i++;
-
-	while (s2[k])
-		k++;
-
-	if (n >= k)
-		l = i + k;
-	else
-		l = i + n;
-
-	str = malloc(sizeof(char) * l + 1);
-	if (str == NULL)
-		return (NULL);
-
-	k = 0;
-	while (j < l)
-	{
-		if (j <= i)
-			str[j] = s1[j];
-
-		if (j >= i)
-		{
-			str[j] = s2[k];
-			k++;
-		}
-		j++;
-	}
-	str[j] = '\0';
-	return (str);
+	return (mem);
 }
